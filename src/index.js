5c3ff2ce57ac1ef94ec021890cfa041278bb043e
Benjamin Seber <seber@synyx.de>
2018-07-20 13:16:58 +0200

    added react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("jeeeehaw");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.createElement("div");
+document.body.appendChild(root);
+
+ReactDOM.render(<div>heyho!</div>, root);
--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,22 @@ module.exports = {
     !isProduction() && new webpack.HotModuleReplacementPlugin(),
   ].filter(Boolean),
 
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, "src"),
+        loader: "babel-loader",
+      },
+    ],
+  },
+
   devServer: {
     port: 8080,
     hot: true,
     noInfo: true,
     after(app) {
+      // eslint-disable-next-line no-console
       console.log("server is running on http://localhost:8080");
     },
   },
