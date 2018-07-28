b6539dba1210a75f84c10887915b4b7d14839cef
Benjamin Seber <seber@synyx.de>
2018-07-28 17:14:19 +0200

    moving a drawn rectangle

--- src/components/RectangleDrawer.jsx
+++ src/components/RectangleDrawer.jsx
@@ -39,6 +39,10 @@ export default class RectangleDrawer extends React.Component {
             y: start.y,
             width: 0,
             height: 0,
+            shadow: {
+              x: start.x,
+              y: start.y,
+            },
           },
         });
       }
@@ -67,6 +71,10 @@ export default class RectangleDrawer extends React.Component {
               y: nextY,
               width: nextWidth,
               height: nextHeight,
+              shadow: {
+                x: nextX,
+                y: nextY,
+              },
             },
           });
         });
@@ -86,6 +94,55 @@ export default class RectangleDrawer extends React.Component {
       this.rootNode.removeEventListener("mousemove", handleMouseMove);
       this.rootNode.removeEventListener("mouseup", handleMouseUp);
     };
+  }
+
+  handleRectangleMove(data) {
+    const { rectangle } = this.state;
+    const { x, y, width, height } = rectangle;
+    let nextX = x + data.dx;
+    let nextY = y + data.dy;
+    if (nextX < 0) {
+      nextX = 0;
+    }
+    if (nextY < 0) {
+      nextY = 0;
+    }
+
+    const boundingClientRect = this.rootNode.getBoundingClientRect();
+    const root = {
+      x: boundingClientRect.x,
+      y: boundingClientRect.y,
+      width: boundingClientRect.width,
+      height: boundingClientRect.height,
+    };
+
+    if (nextX + width > root.width) {
+      nextX = root.x + root.width - width - 1;
+    }
+    if (nextY + height > root.height) {
+      nextY = root.y + root.height - height - 1;
+    }
+
+    this.setState({
+      rectangle: {
+        ...rectangle,
+        shadow: {
+          x: nextX,
+          y: nextY,
+        },
+      },
+    });
+  }
+
+  handleRectangleMoveEnd() {
+    const { rectangle } = this.state;
+    this.setState({
+      rectangle: {
+        ...rectangle,
+        x: rectangle.shadow.x,
+        y: rectangle.shadow.y,
+      },
+    });
   }
 
   componentWillUnmount() {
@@ -110,10 +167,12 @@ export default class RectangleDrawer extends React.Component {
           >
             <svg width="100%" height="100%">
               <Rect
-                x={rectangle.x}
-                y={rectangle.y}
+                x={rectangle.shadow.x}
+                y={rectangle.shadow.y}
                 width={rectangle.width}
                 height={rectangle.height}
+                onMove={data => this.handleRectangleMove(data)}
+                onMoveEnd={() => this.handleRectangleMoveEnd()}
               />
             </svg>
           </div>
@@ -124,6 +183,16 @@ export default class RectangleDrawer extends React.Component {
 }
 
 class Rect extends React.Component {
+  static propTypes = {
+    onMove: PropTypes.func,
+    onMoveEnd: PropTypes.func,
+  };
+
+  static defaultProps = {
+    onMove: () => {},
+    onMoveEnd: () => {},
+  };
+
   state = {
     selected: false,
   };
@@ -131,6 +200,28 @@ class Rect extends React.Component {
   handleRectClick(event) {
     event.stopPropagation();
     this.setState({ selected: !this.state.selected });
+  }
+
+  handleMouseDown(event) {
+    this.start = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+  }
+
+  handleMouseUp(event) {
+    this.start = null;
+    this.props.onMoveEnd();
+  }
+
+  handleMouseMove(event) {
+    if (this.start) {
+      event.stopPropagation();
+      this.props.onMove({
+        dx: event.clientX - this.start.x,
+        dy: event.clientY - this.start.y,
+      });
+    }
   }
 
   render() {
@@ -145,6 +236,9 @@ class Rect extends React.Component {
         onClick={event => this.handleRectClick(event)}
         stroke={selected ? "purple" : "blue"}
         fill="rgba(0,0,0,0.8)"
+        onMouseDown={event => this.handleMouseDown(event)}
+        onMouseUp={event => this.handleMouseUp(event)}
+        onMouseMove={event => this.handleMouseMove(event)}
       />
     );
   }
